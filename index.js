a27a8c19ef5275440a0925162ccdeb2d2127bd10
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-14 21:46:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// On Mouse Click
+var clicked = document.querySelectorAll("button");
+for (var i = 0; i < 7; i++) {
+  clicked[i].addEventListener("click", function () {
+    var content = this.innerHTML;
+    makeSound(content);
+    buttonAnimation(content);
+  });
+}
+
+// On Keyboard Press
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  console.log(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+  }
+}
+
+function buttonAnimation(text) {
+  var content = "." + text;
+  document.querySelector(content).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector(content).classList.remove("pressed");
+  }, 100);
+}
